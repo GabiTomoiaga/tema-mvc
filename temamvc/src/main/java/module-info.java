2d45b05525409b63b96de4bdf78d82b04fb40aa0
Gabi <gabi@gmail.com>
2025-08-25 20:29:01 +0300

    Aplicatie

--- temamvc/src/main/java/module-info.java
+++ temamvc/src/main/java/module-info.java
@@ -2,8 +2,6 @@ module com.example.temamvc {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires org.kordamp.bootstrapfx.core;
     requires java.sql;
 
     opens com.example.temamvc to javafx.fxml;
